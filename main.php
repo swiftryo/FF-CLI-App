74ae4189445e74c50936e6a828aac8e48aa7786a
EC2 Default User <ec2-user@ip-172-31-10-64.ap-northeast-1.compute.internal>
2021-02-02 07:47:54 +0000

    add 3vs3

--- main.php
+++ main.php
@@ -9,36 +9,106 @@
 require_once('./classes/Human.php');
 require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
+require_once('./classes/BlackMage.php');
+require_once('./classes/WhiteMage.php');
+
+$members = array();
+$members[] = new Brave('ティーダ');
+$members[] = new WhiteMage('ユウナ');
+$members[] = new BlackMage('ルールー');
+
+$enemies = array();
+$enemies[] = new Enemy('ゴブリン', 20);
+$enemies[] = new Enemy('ボム', 25);
+$enemies[] = new Enemy('モルボル', 30);
 
 // 設計図を使って大工さんが木材などを組み立てる作業=インスタンス化
 // インスタンス化  new[クラス名]キーワードを使う
-$tiida = new Brave("ティーダ");
 // Humanクラスのインスタンスを$tiidaに代入している感じ
-$goblin = new Enemy("ゴブリン");
 
 
 $turn = 1;
 
 // どちらかのHPが0になるまで繰り返す
-while ($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
-    echo "★★★$turn ターン目 ★★★\n\n";
+ $isFinishFlg = false;
+
+ while (!$isFinishFlg) {    
+     echo "★★★$turn ターン目 ★★★\n\n";
     // ステータス表示
-    echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
-    echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n"; 
-    echo "\n";
+     foreach ($members as $member) {
+         echo $member->getName() . "　：　" . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+     }
+     echo "\n";
+     foreach ($enemies as $enemy) {
+         echo $enemy->getName() . "　：　" . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+     }
+     echo "\n";
     // $tiidaからnameやhitpointを参照している 
     // constで定義したものはオブジェクト定数というものに ::　参照時注意
     
     // 攻撃
-    $tiida->doAttack($goblin);
+     foreach ($members as $member) {
+         $enemyIndex = rand(0, count($enemies) - 1); // 添字は0から始まるので、-1する
+         $enemy = $enemies[$enemyIndex];
+         // 白魔道士の場合、味方のオブジェクトも渡す
+         if (get_class($member) == "WhiteMage") {
+             $member->doAttackWhiteMage($enemy, $member);
+         } else {
+             $member->doAttack($enemy);
+         }
+        echo "\n";
+     }
+     echo "\n";
+     foreach ($enemies as $enemy) {
+         $memberIndex = rand(0, count($members) - 1); // 添字は0から始まるので、-1する
+         $member = $members[$memberIndex];
+         $enemy->doAttack($member);
+         echo "\n";
+     }
     echo "\n";
-    $goblin->doAttack($tiida);
-    echo "\n";
+    
+    
+    // 仲間の全滅チェック
+    $deathCnt = 0; // HPが0以下の仲間の数
+    foreach ($members as $member) {
+        if ($member->getHitPoint() > 0) {
+            $isFinishFlg = false;
+            break;
+        }
+        $deathCnt++;
+    }
+    if ($deathCnt === count($members)) {
+        $isFinishFlg = true;
+        echo "GAME OVER ....\n\n";
+        break;
+    }
+    
+    // 敵の全滅チェック
+    $deathCnt = 0; // HPが0以下の敵の数
+    foreach ($enemies as $enemy) {
+        if ($enemy->getHitPoint() > 0) {
+            $isFinishFlg = false;
+            break;
+        }
+        $deathCnt++;
+    }
+    if ($deathCnt === count($enemies)) {
+        $isFinishFlg = true;
+        echo "♪♪♪ファンファーレ♪♪♪\n\n";
+        break;
+    }
     
     $turn++;
     // ターンを1増やす
 }
 
+
+
 echo "★★★戦闘終了★★★\n\n";
-echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n"; 
-echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n\n"; 
+ foreach ($members as $member) {
+     echo $member->getName() . "　：　" . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+ }
+ echo "\n";
+ foreach ($enemies as $enemy) {
+     echo $enemy->getName() . "　：　" . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+ } 
